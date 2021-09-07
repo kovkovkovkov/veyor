3f6174e0cd7cd1e4d442916c9ba12fd2421a6fd3
Theodore Koveos <z5258231@ad.unsw.edu.au>
2021-09-07 21:07:57 +1000

    added tests

--- q2/Solution.java
+++ q2/Solution.java
@@ -1,5 +1,8 @@
 import java.io.*;
 
+import org.junit.Test;
+import org.junit.Assert.AssertEquals;
+
 public class Solution {
     // Complete the maxMoney function below.
     static int maxMoney(int n, long k) {
@@ -51,7 +54,96 @@ public class Solution {
         // bufferedReader.close();
         // bufferedWriter.close();
     }
+
+    @Test
+    public void testnMin() 
+    {
+        int n = 1; 
+        long k = 2;
+        assertEquals(1, maxMoney(n, k));
+    }
+
+    @Test
+    public void testnMinMinus1() 
+    {
+        int n = 0; 
+        long k = 2;
+        assertEquals(-1, maxMoney(n, k));
+    }
+
+    @Test
+    public void testnMax() 
+    {
+        int n = 2000000000;
+        long k = 2;
+        assertEquals(TODO, maxMoney(n, k));
+    }
+
+    @Test
+    public void testnMaxPlus1() 
+    {
+        int n = 2000000001;
+        long k = 2;
+        assertEquals(-1, maxMoney(n, k));
+    }
+
+    @Test
+    public void testkMin() 
+    {
+        int n = 1; 
+        long k = 1;
+        assertEquals(0, maxMoney(n, k));
+    }
+
+    @Test
+    public void testkMinMinus1() 
+    {
+        int n = 1; 
+        long k = 0;
+        assertEquals(-2, maxMoney(n, k));
+    }
+
+    @Test
+    public void testkMax() 
+    {
+        int n = 1;
+        long k = (long) (4 * Math.pow(10, 15));
+        assertEquals(TODO, maxMoney(n, k));
+    }
+
+    @Test
+    public void testkMaxPlus1() 
+    {
+        int n = 1;
+        long k = (long) (4 * Math.pow(10, 15) + 1);
+        assertEquals(-2, maxMoney(n, k));
+    }
+    
+    @Test
+    public void test1() 
+    {
+        int n = 2;
+        long k = 2;
+        assertEquals(3, maxMoney(n, k));
+    }
+
+    @Test
+    public void test2() 
+    {
+        int n = 2;
+        long k = 2;
+        assertEquals(3, maxMoney(n, k));
+    }
+
+    @Test
+    public void test3() 
+    {
+        int n = 3;
+        long k = 3;
+        assertEquals(5, maxMoney(n, k));
+    }
 }
+
 
 /**
 Tests:
